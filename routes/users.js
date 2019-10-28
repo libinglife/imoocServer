b2c00bd4cfc9a738b2f993673a8416bd34dea042
libing1 <18317776637@163.com>
2019-10-28 22:47:58 +0800

    增加 退出，校验登陆接口

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource ddd');
 });
 
+//登陆
 router.post('/login',(req,res,next)=>{
   let param = {
       userName:req.body.userName,
       userPwd:req.body.userPwd
-  }
+  };
   userModel.findOne(param,(err,userDoc)=>{
     if(err){
       res.json({
@@ -20,23 +21,58 @@ router.post('/login',(req,res,next)=>{
           msg:err.message
       })
     }else {
-      res.cookie("userId",{
+      res.cookie("userId",userDoc.userId,{
           path:'/',
           maxAge:1000*60*60
       });
-      res.cookie("userName",{
+      res.cookie("userName",userDoc.userName,{
           path:"/",
           maxAge:1000*60*60
       });
       res.json({
           status:"0",
           msg:'',
-          resuilt:{
+          result:{
             userName:userDoc.userName
           }
       })
     }
   })
 });
+
+//退出
+router.post('/logout',(req,res,next)=>{
+   res.cookie("userId",'',{
+      path:'/',
+      maxAge:-1
+   });
+   res.json({
+       status:'0',
+       msg:'退出成功',
+       result:""
+   })
+});
+
+//校验登陆
+router.post('/checkLogin',(req,res,next)=>{
+    // console.log("----------------");
+    // console.log(req);
+    if(req.cookies.userId){
+      res.json({
+          status:'0',
+          msg:'登陆成功',
+          result:{
+            userName:req.cookies.userName || ""
+          }
+      })
+    }else {
+        res.json({
+            status:'1',
+            msg:'暂未登陆',
+            result:{}
+        })
+    }
+});
+
 
 module.exports = router;
